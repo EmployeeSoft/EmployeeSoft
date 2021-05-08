05c99751ebf67e86ad53ff45ebc93612e9c4c22f
tpham2103 <79178817+tpham2103@users.noreply.github.com>
2021-05-08 16:01:38 -0400

    Add skeleton for backend

---
+++ Backend/src/main/java/entity/UserRole.java
@@ -0,0 +1,16 @@
+package entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="")
+public class UserRole {
+}
