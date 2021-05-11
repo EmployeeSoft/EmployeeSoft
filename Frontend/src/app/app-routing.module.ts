eee4c1c6ed8b720e09b0c7b682715581d4aea724
Clark Ngo <clarkngo@gmail.com>
2021-05-10 19:15:30 -0700

    add login form with auth guard

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './common/home';
+import { AuthGuard } from './common/_helpers';
 
-const routes: Routes = [];
+const accountModule = () => import('./common/account/account.module').then(x => x.AccountModule);
+const usersModule = () => import('./common/users/users.module').then(x => x.UsersModule);
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
+    { path: 'account', loadChildren: accountModule },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
