ff1c63a6a74d3ef1ca49f3588954018fadf3cf2c
dingwang <dingwang0921@gmail.com>
2021-05-11 04:29:17 -0700

    Added-Navbar-And-Home-Page

---
+++ Frontend/src/app/employee/employee-home/employee-home.component.ts
@@ -0,0 +1,16 @@
+import {Component, NgModule, OnInit} from '@angular/core';
+import {NavbarComponent} from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-employee-home',
+  templateUrl: './employee-home.component.html',
+})
+
+export class EmployeeHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
