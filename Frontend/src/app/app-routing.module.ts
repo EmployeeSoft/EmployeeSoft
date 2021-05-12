3d1542812a7cda0afa843ab10793ca755d09684d
dingwang <dingwang0921@gmail.com>
2021-05-12 13:33:11 -0700

    changes-to-fix-bug

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
     { path: 'onboard', component: OnboardComponent},
 
     // otherwise redirect to home
-    { path: '**', redirectTo: '' }
+    // { path: '**', redirectTo: '' }
 ];
 
 
--- Frontend/src/app/employee/onboard/onboard.component.ts
+++ Frontend/src/app/employee/onboard/onboard.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {Validators} from '@angular/forms';
 import { FormBuilder, FormGroup, FormControl, FormArray } from '@angular/forms';
-import {UploadService} from '../_services/upload.service';
 
 @Component({
   selector: 'app-onboard',
-  templateUrl: './onboard.component.html',
-  styleUrls: ['./onboard.component.css']
+  templateUrl: './onboard.component.html'
 })
 export class OnboardComponent implements OnInit {
   public formData: any;
   public genderOp: ['Male', 'Female', 'Do not want to answer'];
   public visaOp: ['H1-B', 'L2', 'F1(CPT/OPT)', 'H4'];
   public citizenOrNot: boolean;
+  public hasLicence: boolean;
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
@@ -30,9 +30,6 @@ export class OnboardComponent implements OnInit {
       citizenOrNot: ['', Validators.compose([Validators.required])],
       citizenType: [''],
       visa: this.fb.array([]),
-      // startDate: [''],
-      // endDate: [''],
-      // workAuthDoc: [''],
       hasDriverLicence: ['', Validators.compose([Validators.required])],
       driverLicence: [''],
       refFirstName: [''],
@@ -54,35 +51,43 @@ export class OnboardComponent implements OnInit {
       ])
     });
   }
+  // tslint:disable-next-line:typedef
   get getAvatar() {
     return this.formData.get('avatar') as FormControl;
   }
 
+  // tslint:disable-next-line:typedef
   get getVisa(){
     return this.formData.get('visa') as FormArray;
   }
 
+  // tslint:disable-next-line:typedef
   addVisa() {
     this.citizenOrNot = false;
     const visa = this.getVisa;
     visa.clear();
     const content = this.fb.group({
-      type: ['', Validators.required],
-      length: ['', Validators.required],
+      workAuthType: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      workAuthDoc: ['', Validators.required],
     });
     visa.insert(0, content);
   }
 
+  // tslint:disable-next-line:typedef
   deleteVisa() {
     this.citizenOrNot = true;
     const visa = this.getVisa;
     visa.clear();
   }
 
+  // tslint:disable-next-line:typedef
   onSubmit(from: any) {
     console.log(from);
   }
 
+  // tslint:disable-next-line:typedef
   fileChange(event: any, c: FormControl) {
     const reader = new FileReader();
 
