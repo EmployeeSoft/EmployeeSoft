f862cffd3bec5ce04e9a41ddac34e12fbded1c8e
Clark Ngo <clarkngo@gmail.com>
2021-05-16 17:26:35 -0700

    hide visa status mgmt, employee home checks local storage instead of requesting everything

--- Frontend/src/app/navbar/navbar.component.ts
+++ Frontend/src/app/navbar/navbar.component.ts
@@ -11,10 +11,10 @@ export class NavbarComponent implements OnInit {
   constructor(private accountService: AccountService) { }
 
   user: any;
-  user-info: any;
+  userInfo: any;
   ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem("user")!);
-    this.user-info = JSON.parse(localStorage.getItem("user-info")!);
+    this.userInfo = JSON.parse(localStorage.getItem("user-info")!);
     console.log(this.user);
   }
 
